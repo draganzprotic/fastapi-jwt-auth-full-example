223db4c85ece9b8bc961cf7fc1ccd22b58e8afc0
draganzprotic <dragan.z.protic@gmail.com>
2026-05-06 13:24:01 +0200

    add static route for frontend

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.staticfiles import StaticFiles  # Dodaj ovo
 
 from src.routers import router
 
 
 app = FastAPI(default_response_class=ORJSONResponse)
-
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
 app.include_router(router)
